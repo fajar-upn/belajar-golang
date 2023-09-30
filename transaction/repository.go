27fb306983f69803c60efa5c77fc587fcac77ee6
Faja.rS <muhammadfajar1183@gmail.com>
2023-09-30 22:17:56 +0700

    create: transaction repository

---
+++ transaction/repository.go
@@ -0,0 +1,26 @@
+package transaction
+
+import "gorm.io/gorm"
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepoitory(db *gorm.DB) repository {
+	return repository{db}
+}
+
+type Repository interface {
+	GetByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
+	var transaction []Transaction
+
+	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Find(&transaction).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, nil
+}
