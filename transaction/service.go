16bd61f8fd542a3342953e30b9036add2cb04be1
fajar <muhammadfajar1183@gmail.com>
2023-10-15 18:04:15 +0700

    update: from rog's laptop

--- transaction/service.go
+++ transaction/service.go
@@ -1,11 +1,17 @@
 package transaction
 
+import (
+	"bwastartup/campaign"
+	"errors"
+)
+
 type service struct {
-	repository Repository
+	repository         Repository
+	campaignRepository campaign.Repository
 }
 
-func NewService(repository Repository) *service {
-	return &service{repository}
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
 }
 
 type Service interface {
@@ -13,6 +19,16 @@ type Service interface {
 }
 
 func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionInput, userID int) ([]Transaction, error) {
+
+	campaign, err := s.campaignRepository.FindById(input.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return nil, errors.New("This user not owner of the campign")
+	}
+
 	transaction, err := s.repository.GetByCampaignID(input.ID, userID)
 	if err != nil {
 		return nil, err
